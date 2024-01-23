a0c1aedb1d8b2b628e5cba8e7959dfb6d632882d
ShadowNearby <yan.jingsheng@outlook.com>
2024-01-23 21:36:43 +0800

    add concurrent stack test

--- test/stack/concurrent_stack_test.cpp
+++ test/stack/concurrent_stack_test.cpp
@@ -38,6 +38,80 @@ TEST(ConcurrentStackTest, ConcurrentPushAndPop) {
   t2.join();
 }
 
+TEST(ConcurrentStackTest, ConcurrentPush) {
+  ConcurrentStack<int> stack;
+  int num = 10000;
+  std::thread t1([&stack, num]() {
+    for (int i = 0; i < num; ++i) {
+      stack.Push(i);
+    }
+  });
+  std::thread t2([&stack, num]() {
+    for (int i = num; i < num * 2; ++i) {
+      stack.Push(i);
+    }
+  });
+  t1.join();
+  t2.join();
+  ASSERT_EQ(stack.Size(), num * 2);
+}
+
+TEST(ConcurrentStackTest, ConcurrentPushAndPop2) {
+  ConcurrentStack<int> stack;
+  int num = 10000;
+  std::thread t1([&stack, num]() {
+    for (int i = 0; i < num; ++i) {
+      stack.Push(i);
+    }
+  });
+  std::thread t2([&stack, num]() {
+    for (int i = num; i < num * 2; ++i) {
+      stack.Push(i);
+    }
+  });
+  std::thread t3([&stack, num]() {
+    for (int i = 0; i < num; ++i) {
+      stack.WaitAndPop();
+    }
+  });
+  t1.join();
+  t2.join();
+  t3.join();
+  ASSERT_EQ(stack.Size(), num);
+}
+
+TEST(ConcurrentStackTest, ConcurrentPushAndPop3) {
+  ConcurrentStack<int> stack;
+  int num = 100;
+  std::thread t1([&stack, num]() {
+    for (int i = 0; i < num * 2; ++i) {
+      stack.Push(i);
+      std::this_thread::sleep_for(std::chrono::microseconds(10));
+    }
+  });
+  std::thread t2([&stack, num]() {
+    for (int i = 0; i < num * 2; ++i) {
+      stack.Push(i);
+      std::this_thread::sleep_for(std::chrono::microseconds(10));
+    }
+  });
+  std::thread t3([&stack, num]() {
+    for (int i = 0; i < num; ++i) {
+      ASSERT_NE(nullptr, stack.WaitAndPop());
+    }
+  });
+  std::thread t4([&stack, num]() {
+    for (int i = 0; i < num; ++i) {
+      ASSERT_NE(nullptr, stack.WaitAndPop());
+    }
+  });
+  t1.join();
+  t2.join();
+  t3.join();
+  t4.join();
+  ASSERT_EQ(stack.Size(), num * 2);
+}
+
 TEST(ConcurrentStackTest, TryPop) {
   ConcurrentStack<int> stack;
   int num = 10;
